ea1ce7cd077643eae0695ae646466986370c27db
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-02-23 02:07:40 +0600

    add: single blog details

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,9 +1,66 @@
-const page = () => {
-    return (
-        <div>
-            
+import { Metadata } from "next";
+import Image from "next/image";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}): Promise<Metadata> {
+  // read route params
+  const { blogId } = await params;
+
+  // fetch data
+  const data = await fetch(`http://localhost:3004/blogs/${blogId}`);
+  const blog = await data.json();
+
+  return {
+    title: blog?.title,
+    description: blog?.content,
+  };
+}
+
+const page = async ({ params }: { params: Promise<{ blogId: string }> }) => {
+  const { blogId } = await params;
+
+  const data = await fetch(`http://localhost:3004/blogs/${blogId}`, {
+    cache: "no-store",
+  });
+
+  const blog = await data.json();
+  return (
+    //   <!-- Blog post with featured image and dark mode support -->
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 dark:bg-gray-900 my-12 pb-16 py-4 rounded dark:text-gray-100">
+      <div className="max-w-3xl mx-auto">
+        {/* <!-- Blog post header --> */}
+        <div className="py-8">
+          <h1 className="text-3xl font-bold mb-2 dark:text-white">
+            {blog?.title}
+          </h1>
+          <p className="text-gray-500 text-sm dark:text-gray-400">
+            Published on {blog?.date}
+          </p>
         </div>
-    );
+
+        {/* <!-- Featured image --> */}
+        <Image
+          width={200}
+          height={200}
+          src={blog?.image}
+          alt="Featured image"
+          className="w-full h-auto mb-8 rounded-lg shadow-md"
+        />
+        <p className="text-gray-700 dark:text-gray-200 text-xl my-2 mb-8">
+          Written By:
+          <span className="text-blue-500"> {blog?.author}</span>
+        </p>
+
+        {/* <!-- Blog post content --> */}
+        <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl mx-auto dark:prose-invert text-justify">
+          <p>{blog?.content}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,4 +1,6 @@
+import { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
 
 export interface Blog {
   id: number;
@@ -11,13 +13,17 @@ export interface Blog {
   tags: string[];
 }
 
+export const metadata: Metadata = {
+  title: "Arfan Ahmed | Blogs",
+  description: "Arfan Ahmed Blogs",
+};
+
 const BlogPage = async () => {
   const data = await fetch("http://localhost:3004/blogs", {
     cache: "no-store",
   });
 
   const blogs = await data.json();
-  console.log(blogs);
   return (
     <div className="my-20 px-4 py-4 rounded md:px-10 lg:px-20 dark:bg-neutral-900 dark:text-white min-h-screen">
       <h2 className="text-3xl md:text-4xl font-bold text-center mb-10">
@@ -25,26 +31,25 @@ const BlogPage = async () => {
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {blogs?.map((blog: Blog) => (
-          <div
-            key={blog.id}
-            className="rounded-lg shadow-lg bg-white dark:bg-neutral-800 dark:border dark:border-gray-700 p-4 group"
-          >
-            <Image
-              width={200}
-              height={200}
-              src={blog?.image}
-              alt={blog?.title}
-              className="w-full h-48 object-cover rounded-lg group-hover:scale-110 duration-1000"
-            />
-            <div className="flex items-center text-sm text-gray-600 dark:text-gray-400 mt-3">
-              <span>{blog?.date}</span>
-              <span className="mx-2">|</span>
-              <span>{blog?.author}</span>
+          <Link href={`/blogs/${blog?.id}`} key={blog.id}>
+            <div className="rounded-lg shadow-lg bg-white dark:bg-neutral-800 dark:border dark:border-gray-700 p-4 group">
+              <Image
+                width={200}
+                height={200}
+                src={blog?.image}
+                alt={blog?.title}
+                className="w-full h-48 object-cover rounded-lg group-hover:scale-105 duration-1000"
+              />
+              <div className="flex items-center text-sm text-gray-600 dark:text-gray-400 mt-3">
+                <span>{blog?.date}</span>
+                <span className="mx-2">|</span>
+                <span>{blog?.author}</span>
+              </div>
+              <h3 className="text-lg font-semibold text-gray-800 dark:text-white hover:underline mt-2">
+                {blog?.title}
+              </h3>
             </div>
-            <h3 className="text-lg font-semibold text-gray-800 dark:text-white hover:underline mt-2">
-             {blog?.title}
-            </h3>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
