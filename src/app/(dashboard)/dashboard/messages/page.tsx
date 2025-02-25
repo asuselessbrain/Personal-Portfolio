399517b7f08d1f5640debb38c3181874ecf88c32
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-02-25 19:53:09 +0600

    add: show message in dashboard

--- src/app/(dashboard)/dashboard/messages/page.tsx
+++ src/app/(dashboard)/dashboard/messages/page.tsx
@@ -1,10 +1,56 @@
-
-const MessagesPage = () => {
-    return (
-        <div>
-            Messages Page
-        </div>
-    );
+type Message = {
+  id: string;
+  name: string;
+  email: string;
+  message: string;
+};
+const MessagesPage = async () => {
+  const data = await fetch("http://localhost:3004/contacts", {
+    cache: "no-store",
+  });
+  const messages = await data.json();
+  console.log(messages);
+  return (
+    <div className="w-full m-10">
+      <h2 className="text-3xl md:text-6xl text-center mb-10 font-bold">
+        Messages
+      </h2>
+      <div className="relative overflow-x-auto max-h-[90vh]">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Message
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {messages?.map((message: Message) => (
+              <tr
+                key={message.id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
+              >
+                <td
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {message.name}
+                </td>
+                <td className="px-6 py-4">{message.email}</td>
+                <td className="px-6 py-4">{message.message}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MessagesPage;
